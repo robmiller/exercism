eb55dd53739808ef7fcaccfcf5d7f7ea0828853b
Rob Miller <rob@bigfish.co.uk>
2013-08-18 15:32:43 +0100

    Rev #6 of word-count

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -3,28 +3,17 @@ class Phrase
     @text = text.to_s
   end
 
-  def ignoring_case
-    original_text = @text.dup
-    @text.downcase!
-
-    result = yield
-
-    @text = original_text
-
-    result
+  def normalise
+    @text.downcase
   end
 
   def split_words
-    @text.scan(/\w+/)
+    normalise.scan(/\w+/)
   end
 
   def word_count
-    ignoring_case do
-      @words = split_words
-
-      @words.each_with_object(Hash.new(0)) do |word, counts|
-        counts[word] += 1
-      end
+    split_words.each_with_object(Hash.new(0)) do |word, counts|
+      counts[word] += 1
     end
   end
 end
